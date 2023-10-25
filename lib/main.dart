64e62f4cba30bf7a5d03e696d958a3ea7b9efaf1
heshoo69 <62772990+heshoo69@users.noreply.github.com>
2023-10-25 22:38:05 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: null,
+    body: SafeArea(
+      child: WebView(
+    initialUrl: 'https://google.com', // Replace with your website URL
+    javascriptMode: JavascriptMode.unrestricted, // Enable JavaScript
+    ),
+    ),
+    ),
+    );
+  }
+}
